cfaff3d6a4d97b77d353201f00499569b399a3a0
maryam <fatemehdabirian684@gmail.com>
2020-12-26 12:28:55 +0330

    note

---
+++ app/src/main/java/com/example/androidnote/model.java
@@ -0,0 +1,14 @@
+package com.example.androidnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class model extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model);
+    }
+}
